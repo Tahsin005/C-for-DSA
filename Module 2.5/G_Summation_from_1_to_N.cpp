df5409f801d1fc1ef74b132b481a3caacc4519f2
Tahsin005 <tahsin.ferdous3546@gmail.com>
2023-06-07 00:32:27 +0600

    C++ for dsa

--- Module 2.5/G_Summation_from_1_to_N.cpp
+++ Module 2.5/G_Summation_from_1_to_N.cpp
@@ -3,9 +3,7 @@ using namespace std;
 int main(){
     long long int n,sum=0;
     cin>>n;
-    for(int i=1;i<=n;i++){
-        sum=sum+i;
-    }
+    sum=(n*(n+1)/2);
     cout<<sum;
     return 0;
 }
